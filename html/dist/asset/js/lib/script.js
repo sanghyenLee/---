fbd05a492020e305429f8fa96d533a576468a4c8
sanghyenLee <alikcz83@gmail.com>
2023-11-10 16:58:19 +0900

    1110last

---
+++ html/dist/asset/js/lib/script.js
@@ -0,0 +1,23 @@
+// $(function(){
+//     var $firstmenu = $('.menu > ul > li > a'),
+//         $header = $('.hoverMenu');
+//      $firstmenu.mouseenter(function(){
+//         $header.stop().animate({height:'372px', opacity: '1'}, 1000);
+//      })
+//      .mouseleave(function(){
+//          $header.stop().animate({ opacity: '0'},1000);
+//      })
+     
+     
+//  });
+
+// $(function(){
+//     var $sitemap = $('.menu > sitemap-icon > a'),
+//         $header = $('.hoverMenu');
+//     $sitemap.mouseenter(function(){
+//         $header.stop().animate({height:'372px', opacity: '1'}, 1000);
+//      })
+//      .mouseleave(function(){
+//         $header.stop().animate({ opacity: '0'},1000);
+//     })
+// });
