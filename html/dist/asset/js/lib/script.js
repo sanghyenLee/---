ddeca516325bc747f61167ff6c7a0933f649a64e
sanghyenLee <alikcz83@gmail.com>
2023-11-13 16:21:47 +0900

    header

--- html/dist/asset/js/lib/script.js
+++ html/dist/asset/js/lib/script.js
@@ -1,23 +1,32 @@
-// $(function(){
-//     var $firstmenu = $('.menu > ul > li > a'),
-//         $header = $('.hoverMenu');
-//      $firstmenu.mouseenter(function(){
-//         $header.stop().animate({height:'372px', opacity: '1'}, 1000);
-//      })
-//      .mouseleave(function(){
-//          $header.stop().animate({ opacity: '0'},1000);
-//      })
-     
-     
-//  });
+$(document).ready(function(){
 
-// $(function(){
-//     var $sitemap = $('.menu > sitemap-icon > a'),
-//         $header = $('.hoverMenu');
-//     $sitemap.mouseenter(function(){
-//         $header.stop().animate({height:'372px', opacity: '1'}, 1000);
-//      })
-//      .mouseleave(function(){
-//         $header.stop().animate({ opacity: '0'},1000);
-//     })
-// });
+    $('#menufloat .listbox').hide();
+    $('.menubk').hide();
+
+    $('.bold').mouseenter(function(){
+        timer = setTimeout(function() {       
+            $(".listbox").not(':animated')      
+            .slideDown(500)                     
+         }, 100),
+         timer = setTimeout(function() {       
+            $(".menubk").not(':animated')      
+            .slideDown(500)                     
+         }, 100)
+         timer = setTimeout(function() {       
+            $(".MenuContent").not(':animated') 
+            .css({background: "white"})                        
+         }, 100)
+       
+    })
+
+    $('.menufloat').mouseleave(function(){
+        clearTimeout(timer);         
+		$('.listbox').slideUp(500),
+        $('.menubk').slideUp(500);
+        timer = setTimeout(function() {       
+            $(".MenuContent").not(':animated') 
+            .css({background: "none"})                        
+         }, 500)
+	});
+
+})
