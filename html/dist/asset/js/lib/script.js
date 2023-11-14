57a4f992e1b718b0a67691b220f02625cc31f600
sanghyenLee <alikcz83@gmail.com>
2023-11-14 12:14:24 +0900

    header complete

--- html/dist/asset/js/lib/script.js
+++ html/dist/asset/js/lib/script.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".bold").hover(function () {
+    timer = setTimeout(function () {
+      $("header").not(":animated").css({ overflow: "visible" });
+      $(".bold").not(":animated").css({ color: "black" });
+      $(".HeaderMain").not(":animated").css({ background: "#ffffff" });
+      $(".headerlogoh").not(":animated").css({ opacity: "1" });
+      $(".sitemaph").not(":animated").css({ opacity: "1" });
+    }, 100);
+  });
 
-    $('#menufloat .listbox').hide();
-    $('.menubk').hide();
+  $(".menufloat").mouseleave(function () {
+    // $(".listbox").slideUp(500), $(".menubk").slideUp(500);
+    timer = setTimeout(function () {
+      $("header").not(":animated").css({ overflow: "hidden" });
+      $(".bold").not(":animated").css({ color: "#ffffff" });
+      $(".HeaderMain").not(":animated").css({ background: "" });
+      $(".headerlogoh").not(":animated").css({ opacity: "0" });
+      $(".sitemaph").not(":animated").css({ opacity: "0" });
+    }, 100);
+  });
 
-    $('.bold').mouseenter(function(){
-        timer = setTimeout(function() {       
-            $(".listbox").not(':animated')      
-            .slideDown(500)                     
-         }, 100),
-         timer = setTimeout(function() {       
-            $(".menubk").not(':animated')      
-            .slideDown(500)                     
-         }, 100)
-         timer = setTimeout(function() {       
-            $(".MenuContent").not(':animated') 
-            .css({background: "white"})                        
-         }, 100)
-       
-    })
-
-    $('.menufloat').mouseleave(function(){
-        clearTimeout(timer);         
-		$('.listbox').slideUp(500),
-        $('.menubk').slideUp(500);
-        timer = setTimeout(function() {       
-            $(".MenuContent").not(':animated') 
-            .css({background: "none"})                        
-         }, 500)
-	});
-
-})
+  $(".sitemaph").click(function () {
+    timer = setTimeout(function () {
+      $("header").not(":animated").css({ overflow: "visible" });
+      $(".bold").not(":animated").css({ color: "black" });
+      $(".HeaderMain").not(":animated").css({ background: "#ffffff" });
+      $(".headerlogoh").not(":animated").css({ opacity: "1" });
+      $(".sitemaph").not(":animated").css({ opacity: "1" });
+    }, 100);
+  });
+});
